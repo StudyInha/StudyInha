1453e5f45be88a473a3488ee1eed5c043e84415d
hhhan-jh <jeo4924@gmail.com>
2025-10-30 04:39:58 +0900

    Feat: 판매 페이지 이원화
    아직 판매글이 없어서 휑한 것을 보여주는 것보단 대체 페이지를 하나 두어 쉽게 바꿀 수 있게 하는 것이 좋겠다 판단

--- src/pages/SalePage.js
+++ src/pages/SalePage.js
@@ -4,7 +4,8 @@ const SalePage = () => {
   return (
     <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src="https://leaf-target-bf5.notion.site/ebd/29bbe199511b80feb694c0827e6b98bb" 
+        // src="https://leaf-target-bf5.notion.site/ebd/29bbe199511b80a0b58efbcdd24c006f" // 아직 준비중입니다
+        src="https://leaf-target-bf5.notion.site/ebd/29bbe199511b80feb694c0827e6b98bb" // 입점 리스트
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="Notion Page"
         allowFullScreen
