94ae89bdd422505cc4e21f9da1e42ed3296aa4f8
hhhan-jh <jeo4924@gmail.com>
2026-02-12 17:11:06 +0900

    HOTFIX: 노션 임베딩 안되는 오류 수정

--- src/pages/SalePage.js
+++ src/pages/SalePage.js
@@ -4,7 +4,8 @@ const SalePage = () => {
   return (
     <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src="https://leaf-target-bf5.notion.site/ebd/2b1be199511b80b38752fe2465dc07c6" 
+        // src="https://leaf-target-bf5.notion.site/ebd/2b1be199511b80b38752fe2465dc07c6" 
+        src="https://v2-embednotion.com/2b1be199511b80b38752fe2465dc07c6"
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="Notion Page"
         allowFullScreen
